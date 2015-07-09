595c3c12b2caeb107067e471540fc8bce7e7820d
nicklovell23 <nicklovell23@gmail.com>
2015-07-09 17:28:38 -0500

    Revert "Squash Update GPS libs to Lollipop and add GPS location service use tmo GPS blobs."
    This reverts commit c7ea25991bfab23622ba5264fb13ee4e2761070b.

--- trlteusc/Android.mk
+++ trlteusc/Android.mk
@@ -17,6 +17,16 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),trlteusc)
-# Leave this here for later.
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
+
 endif
 
